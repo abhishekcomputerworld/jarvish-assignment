77406c543a3562511a6ffda6a08cd7ec495a9d9b
Abhishek Pandey <abhishek.pandey@redcliffelabs.com>
2023-04-15 01:12:17 +0530

    commit

--- app/src/main/java/com/abhishek/jarvish/fragments/UserListFragment.kt
+++ app/src/main/java/com/abhishek/jarvish/fragments/UserListFragment.kt
@@ -7,14 +7,12 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
 import com.abhishek.jarvish.R
-import com.abhishek.jarvish.databinding.FragmentFirstBinding
+import com.abhishek.jarvish.databinding.FragmentUserListBinding
 
-/**
- * A simple [Fragment] subclass as the default destination in the navigation.
- */
-class FirstFragment : Fragment() {
 
-    private var _binding: FragmentFirstBinding? = null
+class UserListFragment : Fragment() {
+
+    private var _binding: FragmentUserListBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -25,7 +23,7 @@ class FirstFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
 
-        _binding = FragmentFirstBinding.inflate(inflater, container, false)
+        _binding = FragmentUserListBinding.inflate(inflater, container, false)
         return binding.root
 
     }
@@ -33,8 +31,8 @@ class FirstFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.buttonFirst.setOnClickListener {
-            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+        binding.buttonSecond.setOnClickListener {
+            findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
         }
     }
 
